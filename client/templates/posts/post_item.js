666b664eaae4497520aa9d2c6ffd2bb0f1956043
Jon Langel <jlangel32@hotmail.com>
2017-03-28 16:30:28 -0500

    Denormalized the number of comments into the post.

--- client/templates/posts/post_item.js
+++ client/templates/posts/post_item.js
@@ -3,14 +3,17 @@
  */
 
 Template.postItem.helpers({
+
     ownPost: function() {
         return this.userId === Meteor.userId();
     },
+
     domain: function() {
         let a = document.createElement("a");
         a.href = this.url;
         return a.hostname;
     },
+
     commentsCount: function() {
         return Comments.find({postId: this._id}).count();
     }
