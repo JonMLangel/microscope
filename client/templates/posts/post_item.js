cd31586ac18e442eefdf2c3c153dcf0cbe2eafba
Jon Langel <jlangel32@hotmail.com>
2017-03-28 16:31:10 -0500

    Denormalized the number of comments into the post.

--- client/templates/posts/post_item.js
+++ client/templates/posts/post_item.js
@@ -13,10 +13,6 @@ Template.postItem.helpers({
         a.href = this.url;
         return a.hostname;
     },
-
-    commentsCount: function() {
-        return Comments.find({postId: this._id}).count();
-    }
 });
 
 
