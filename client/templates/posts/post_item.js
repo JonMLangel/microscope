4d6e3f9fb352c42bbe7cde9dd6ff331191318563
curtis <cjones@clearviewaudit.com>
2017-03-30 16:18:49 -0500

    Added basic upvoting algorithm

--- client/templates/posts/post_item.js
+++ client/templates/posts/post_item.js
@@ -12,7 +12,14 @@ Template.postItem.helpers({
         let a = document.createElement("a");
         a.href = this.url;
         return a.hostname;
-    },
+        },
+});
+
+Template.postItem.events({
+    'click .upvote': function(e) {
+        e.preventDefault();
+        Meteor.call('upvote', this._id);
+    }
 });
 
 
